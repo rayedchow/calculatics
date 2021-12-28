6ad572a379bda5d62643abb8f2703aceae4b1255
voomp <bat7gamer7@gmail.com>
2021-12-28 18:38:46 -0500

    config fixes

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ import ConfigData from '../config.json';
 const CONFIG_OBJ: Config = ConfigData;
 const CLI_ARGS = process.argv.slice(2);
 
-if(CLI_ARGS.length == 0) {
+if(CLI_ARGS.length == 1) {
 	console.log(CONFIG_OBJ.menu.join('\n'));
 }
