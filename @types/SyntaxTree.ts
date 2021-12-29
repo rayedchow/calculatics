3db17b5fdbd25307ffbdbd2c7046babd31dc5baf
voomp <bat7gamer7@gmail.com>
2021-12-29 18:12:03 -0500

    syntax tree types

--- @types/SyntaxTree.ts
+++ @types/SyntaxTree.ts
@@ -1,2 +1,59 @@
 // abstract syntax tree
 // formed by parser and used by interpreter
+
+type SyntaxBranch = ReturnStatement;
+
+interface ReturnStatement {
+	value?: number,
+	operation?: OperationTree
+}
+
+export interface OperationTree {
+	values: (number | OperationTree | Identifier)[]
+	operator: Operator
+}
+
+interface Identifier {
+	identifier: string
+}
+
+export enum Operator {
+	Add,
+	Subtract,
+	Multiply,
+	Divide
+}
+
+export const SyntaxTree: SyntaxBranch[] = [];
+
+// example syntax tree:
+// [
+//	 {
+//		 type: 'RETURN_STATEMENT',
+//		 value: 3
+//	 }
+// ]
+
+// example operation tree:
+// for operation: (5+39)-(18/m)
+// {
+//		values: [
+//	 		{
+//	 			values: [
+//	 				5,
+//	 				39
+//	 			],
+//	 			operation: 'ADD'
+//	 		},
+//	 		{
+//	 			values: [
+//	 				18,
+//	 				{
+//	 					identifier: 'm'
+//	 				}
+//	 			],
+//	 			operation: 'DIVIDE'
+//	 		}
+//		],
+//		operation: 'SUBTRACT'
+// }
