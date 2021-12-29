b2d5c0c09f1c0f4ca21ea652ed24865e669c7c25
voomp <bat7gamer7@gmail.com>
2021-12-29 16:11:12 -0500

    syntax tree

---
+++ src/interpreter/Parser.ts
@@ -0,0 +1,6 @@
+// ---- The Parser ----
+// The second section of the Calculatics Compiler
+// Loops through tokens and forms the AST struct
+// The AST will be later used and interpreted
+// *** AST = Abstract Syntax Tree ***
+
