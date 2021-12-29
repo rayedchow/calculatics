66413b40d40f7560466c2fb606974bdd8e81d601
voomp <bat7gamer7@gmail.com>
2021-12-28 21:11:43 -0500

    -f arg

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,15 @@ import ConfigData from '../config.json';
 const CONFIG_OBJ: Config = ConfigData;
 const CLI_ARGS = process.argv.slice(2);
 
+// non-arg CLI menu
 if(CLI_ARGS.length == 1) {
 	console.log(CONFIG_OBJ.menu.join('\n'));
 	process.exit();
+}
+
+// parsing file argument
+if(CLI_ARGS[1] == '-f') {
+	if(CLI_ARGS.length == 2) process.exit();
+
+	console.log(CLI_ARGS[2]);
 }
