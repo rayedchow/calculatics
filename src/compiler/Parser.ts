354c7b5056b87707f02629bd17c310d3bc8be888
voomp <bat7gamer7@gmail.com>
2021-12-30 09:16:43 -0500

    parser commenting fixes

--- src/compiler/Parser.ts
+++ src/compiler/Parser.ts
@@ -1,12 +1,11 @@
+import { SyntaxBranch } from "../../@types/SyntaxTree";
+import { Token, TokenType } from "../../@types/Token";
+
 // ---- The Parser ----
 // The second section of the Calculatics Compiler
 // Loops through tokens and forms the AST struct
 // The AST will be later used and interpreted
 // *** AST = Abstract Syntax Tree ***
-
-import { SyntaxBranch } from "../../@types/SyntaxTree";
-import { Token, TokenType } from "../../@types/Token";
-
 export const parse = (tokens: Token[]) => {
 	
 	let expStage = 0;
