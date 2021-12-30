09934d8a66c0a822f06dba64951fd485069ec31f
voomp <bat7gamer7@gmail.com>
2021-12-30 12:42:30 -0500

    prod. cli code

--- src/argHandler.ts
+++ src/argHandler.ts
@@ -1,24 +1,23 @@
 import { Config } from '../@types/Config';
-import { readFile } from './compiler/fileReader';
-import { lex } from './compiler/Lexer';
+import { compile } from './compiler/compile'
 
 // made for CLI (prod.)
-export const handleArgs = (args) => {
+export const handleArgs = (args: string[]) => {
 	// non-arg CLI menu
-	if(args.length == 1) {
+	if(args.length == 0) {
 		console.log(Config.menu.join('\n'));
 		process.exit();
 	}
 
 	// parsing file argument
-	if(args[1] == '-f') {
-		if(args.length == 2) {
+	if(args[0] == '-f') {
+		if(args.length == 1) {
 			console.log('No file location provided (provide after -f arg)');
 			process.exit();
 		}
 
-		const fileDir = `${args[0]}/${args[2]}`;
-		const fileContent = readFile(fileDir);
-		const tokens = lex(fileContent.split(''));
+		// gets file directory and compiles code
+		const fileDir = `${process.cwd()}/${args[1]}`;
+		compile(fileDir);
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-// import { handleArgs } from "./argHandler";
-import { testLibrary } from "./testHandler";
-
 // PRODUCTION CODE:
-// const CLI_ARGS = process.argv.slice(2);
-// handleArgs(CLI_ARGS);
+import { handleArgs } from "./argHandler";
+
+const CLI_ARGS = process.argv.slice(2);
+handleArgs(CLI_ARGS);
 
 // TESTING CODE:
 // - uses testing library to perform
 //	 test cases on .calc files
-testLibrary([
-	null,
-	null
-]);
+// import { testLibrary } from "./testHandler";
+//
+// testLibrary([
+// 	null,
+// 	null
+// ]);
