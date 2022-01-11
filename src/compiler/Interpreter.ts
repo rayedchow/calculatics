0c0333f79111d3a28a21b033ca832724a5bb39dc
voomp <bat7gamer7@gmail.com>
2022-01-11 08:59:50 -0500

    priority operation nested

--- src/compiler/Interpreter.ts
+++ src/compiler/Interpreter.ts
@@ -52,7 +52,10 @@ const priorityOperation = (operationTree: OperationTree, nested: boolean = false
 	let nestOperator = true;
 
 	for(let i = 0; i < operationTree.length; i++) {
-		const operation = operationTree[i];
+		let operation = operationTree[i];
+
+		if((typeof operation === 'object') && (Array.isArray(operation)))
+			operation = priorityOperation(operation);
 
 		if(['*','/'].includes(operation as Operator)) {
 			if(!nested) {
@@ -91,6 +94,7 @@ const evalOperation = (priorityTree: OperationTree, line: number): number => {
 	if(typeof priorityTree[0] === 'string')
 		return handleError('invalid number in operation tree', line, -1);
 	
+	console.log(priorityTree);
 	let firstOperation = priorityTree[0];
 	let currNum: number;
 	if(typeof firstOperation === 'object') {
